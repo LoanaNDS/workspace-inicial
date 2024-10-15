73ef8f67abc7c5c9b6ad882b0cb9f333885447f7
LoanaNDS <lua0796@gmail.com>
2024-10-14 22:23:57 -0300

    MenuDesplegableDelUsuario

---
+++ js/NavBar.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var nombreUsuario = localStorage.getItem("user");
+      
+      if (nombreUsuario) {
+        var navbar = document.querySelector(".navbar-nav");
+        var userItem = document.createElement("li");
+        userItem.classList.add("nav-item");
+        userItem.innerHTML = `<div class="dropdown">
+        <a class="btn btn-success dropdown-toggle" href="my-profile.html"role="button" data-bs-toggle="dropdown" aria-expanded="false">${nombreUsuario}</a>
+        <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="cart.html">Mi Carrito</a></li>
+    <li><a class="dropdown-item" href="my-profile.html">Mi Perfil</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item" href="login.html">Cerrar sesión</a></li>
+  </ul>
+  </div>`;
+        navbar.appendChild(userItem);
+      }
+});
