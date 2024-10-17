6fe3474a424e2e26f426b7fa464500ebf62eed30
xannahc <annahenriquez10@gmail.com>
2024-10-17 19:37:44 -0300

    Tercer y ultimo arreglo validacion email en Login

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const botonIngresar = document.getElementById('botonIngresar');
     const formulario = document.querySelector('form.login');
-  
+    
     botonIngresar.addEventListener('click', (event) => {
     
       event.preventDefault();
@@ -9,9 +9,16 @@ document.addEventListener('DOMContentLoaded', () => {
       
       const usuario = document.getElementById('usuario').value.trim();
       const contrasena = document.getElementById('contrasena').value.trim();
+      const emailValidado = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   
       if (usuario === '' || contrasena === '') {
         alert('Por favor, complete todos los campos.');
+        return
+      
+      } 
+      if (!emailValidado.test(usuario)) {
+        alert('Por favor, ingresa un correo válido');
+      return
       } else {
         window.location.href = 'index.html';
         localStorage.setItem("user",usuario)
